61f28d921412b38769f5f71d52a3b9eba78644ef
Geremsa <geremsanrzr@gmail.com>
2022-08-29 19:15:39 +0530

    prev/next button responsive

--- beats.js
+++ beats.js
@@ -1,12 +1,32 @@
 const music = document.querySelector('audio');
 const play = document.querySelector('.play');
-const pause = document.querySelector('pause');
-const stats = document.getElementsByClassName('svg');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+//music controls
+
+const musicDir = [
+    {
+        name: "FireForce-Op-Inferno",
+    },
+    {
+        name: "MonkeyMajik-Eden",
+    },
+    {
+        name: "Gintama ost pianoVer",
+    },
+    {
+        name: "Nanoka Hara-Suzume no Tojimari",
+    },
+];
+
+let currentMusic = 0;
+music.src = './music/'+musicDir[currentMusic].name+'.mp3';
 
 let isplaying = false;
 play.addEventListener('click', () => {
     //for playing
-        if(isplaying == false){
+    if(isplaying == false){
         music.play();
         isplaying = true;
         play.classList.replace('play', 'pause');
@@ -18,3 +38,44 @@ play.addEventListener('click', () => {
         play.classList.replace('pause', 'play');
     }
 });
+
+next.addEventListener('click', () => {
+    let i = currentMusic;
+    if (currentMusic < musicDir.length - 1) {
+        i = i+1;
+        tonext(i);
+    }else if (currentMusic <= musicDir.length) {
+        i = 0;
+        tonext(i);
+    }else {
+        tonext(currentMusic);
+    }
+    currentMusic = i;
+
+    console.log(currentMusic);
+});
+
+function tonext(next) {
+    music.src = './music/'+musicDir[next].name+'.mp3';
+    music.play();
+    play.classList.replace('play', 'pause');
+}
+
+prev.addEventListener('click', () => {
+    let i = currentMusic;
+    if (currentMusic > 0 && currentMusic < musicDir.length){
+        i = i-1;
+        toprev(i);
+    }else if(currentMusic == 0) {
+        i = musicDir.length - 1;
+        toprev(i);
+    }
+    currentMusic = i;
+    console.log(currentMusic);
+});
+
+function toprev(previous) {
+    music.src = './music/'+musicDir[previous].name+'.mp3';
+    music.play();
+    play.classList.replace('play', 'pause');
+}
